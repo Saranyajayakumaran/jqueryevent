07898509e6a6d63895e10e95079bb51fd51d5953
Saranya <jsaran.bj@gmail.com>
2024-04-10 11:07:29 +0200

    manipulating dom

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+$('button').mouseenter(function(){
+    $(this).removeClass('makeblue').addClass('makeblue');
+
+});
+
+$('button').mouseleave(function(){
+    $(this).removeClass('makered').addClass('makered');
+
+});
